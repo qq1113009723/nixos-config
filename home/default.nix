0e75db0a8a68527bbae52225ef637c050fbab9bb
zhoujinqiu <1113009723@qq.com>
2026-01-06 15:05:15 +0800

    Add Git proxy settings and enable Git in user settings

--- home/default.nix
+++ home/default.nix
@@ -11,5 +11,10 @@
     polkit.enable = true;
     code-cursor.enable = true;
     jetbrains.idea-ultimate.enable = true;
+    git = {
+      enable = true;
+      useProxy = true;
+      proxy = "http://192.168.106.171:7890";
+    };
   };
 }
--- home/modules/git.nix
+++ home/modules/git.nix
@@ -1,12 +1,39 @@
-{ pkgs, ... }:
+{ config, lib, ... }:
 
 {
-  programs.git = {
-    enable = false;
-    settings = {
-      user.name = "zhoujinqiu";
-      user.email = "1113009723@qq.com";
-      https.proxy = "http://192.168.106.171:7890";
+  options.userSettings.git = {
+    enable = lib.mkEnableOption "Enable programs.git";
+    useProxy = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to set git http/https proxy settings";
     };
+    proxy = lib.mkOption {
+      type = lib.types.str;
+      default = "http://192.168.106.171:7890";
+      description = "Proxy URL to use when userSettings.git.useProxy is true";
+    };
+  };
+
+  config = lib.mkIf config.userSettings.git.enable {
+    programs.git.enable = true;
+
+    programs.git.settings = (
+      let 
+          base = {
+            user = {
+              name = "zhoujinqiu";
+              email = "1113009723@qq.com";
+            };
+            credential = {
+              helper = "store";
+            };
+          };
+      in 
+      if config.userSettings.git.useProxy then base // {
+          http = { proxy = config.userSettings.git.proxy; };
+          https = { proxy = config.userSettings.git.proxy; };
+      } else base
+    );
   };
 }
